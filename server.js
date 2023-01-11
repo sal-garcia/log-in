f33b8bb7ddfc57454982d80d7706f65007b07b6a
sal <salgarcia940@gmail.com>
2023-01-11 00:47:41 -0800

    working on server

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ app.get('/users', (req,res)=>{
   res.json(users)
 })
 
-app.post('/users', (req, res) => {
-  const user = {
-    name: req.body.name,
-    password: req.body.password
+app.post('/users', async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    console.log(salt)
+    console.log(hashedPassword)
+    const user = { name: req.body.name, password: hashedPassword }
+    users.push(user)
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
   }
-  users.push(user)
-  res.status(201).send()
 })
 
 
