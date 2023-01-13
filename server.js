f35d3040652a14f6565bf8e97be26dc1d7d7b9af
sal <salgarcia940@gmail.com>
2023-01-12 20:55:09 -0800

    server completed

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/users', (req,res)=>{
 
 app.post('/users', async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt()
-    const hashedPassword = await bcrypt.hash(req.body.password, salt)
-    console.log(salt)
-    console.log(hashedPassword)
+    const salt = await bcrypt.genSalt() //generates salt aka random string to make hash unpredictable
+    const hashedPassword = await bcrypt.hash(req.body.password, salt) //appends salt to hashed password
+    // console.log(salt, 'salt')
+    // console.log(hashedPassword, 'hashedpassword')
     const user = { name: req.body.name, password: hashedPassword }
     users.push(user)
     res.status(201).send()
@@ -24,5 +24,20 @@ app.post('/users', async (req, res) => {
   }
 })
 
+app.post('/users/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name)
+  if (user == null) {
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success passwords do match')// if passwords match
+    } else {
+      res.send('Not Allowed passwords dont match')
+    }
+  } catch {
+    res.status(500).send()
+  }
+})
 
 app.listen(3000)
